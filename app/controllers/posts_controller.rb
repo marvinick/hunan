3755caa8ee381f4cb34f6dc88e1821e573f7060f
marvinick <marviluw@gmail.com>
2015-04-15 19:54:10 -0500

    before_action on post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_member, only: [:new, :create, :destroy, :edit, :update]
 
   def index
     @posts = Post.all
