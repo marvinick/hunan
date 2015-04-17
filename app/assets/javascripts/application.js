9f93960fb8bdeb9a22813fbe0fda1072f75d4fff
marvinick <marviluw@gmail.com>
2015-04-17 16:13:52 -0500

    log added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require angular
+//= require angular-route
+//= require angular-resource
 //= require turbolinks
 //= require_tree .
