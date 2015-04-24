b5a7657b7c25dcb6224e51cbc4391efa8af04cbd
marvinick <marviluw@gmail.com>
2015-04-24 07:21:53 -0500

    header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,16 @@
 //= require turbolinks
 //= require_tree .
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('.navbar');
+   var offset = startchange.offset();
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.navbar').css('background-color', 'rgba(34,34,34,0.9)');
+       } else {
+          $('.navbar').css('background-color', 'transparent');
+       }
+   });
+});
